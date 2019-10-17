b5779bd97c7eb2b7c74acc2d0bf6d5db6d5c9162
NomiJ <nauman.qazi@systematicbytes.com>
2019-10-17 21:00:41 +1100

    View and Controller logic Added

---
+++ cabinet/app/controllers/docs_controller.rb
@@ -0,0 +1,48 @@
+class DocsController < ApplicationController
+
+	before_action :find_doc, only: [:show, :edit, :update, :destroy]
+  def index
+  	@docs = Doc.all.order("created_at DESC")
+  end
+
+  def show
+  end
+
+  def new
+  	@doc = Doc.new
+  end
+
+  def create
+  	  	@doc = Doc.new(doc_param)
+  	  	if @doc.save
+  	  		redirect_to @doc
+  	  	else
+  	  		render 'new'
+  	  	end
+  end
+
+  def edit
+  end
+
+  def destroy
+  	@doc.destroy
+  	redirect_to docs_path
+  end
+
+  def update
+  	if @doc.update(doc_param)
+  	  		redirect_to @doc
+  	  	else
+  	  		render 'edit'
+  	  	end
+  end
+
+private 
+  def find_doc
+  	  	@doc = Doc.find(params[:id])
+  end
+
+  def doc_param
+  	params.require(:doc).permit(:title, :content)
+  end
+end
