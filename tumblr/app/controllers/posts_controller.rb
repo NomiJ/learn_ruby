b6ccfb60d26188171894adc6689b8ff96fb5aa60
NomiJ <nauman.qazi@systematicbytes.com>
2019-10-21 20:26:37 +1100

    Add before_action

--- tumblr/app/controllers/posts_controller.rb
+++ tumblr/app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
 
+        before_action :find_post, only: [:show, :edit, :update, :destroy]
+
 
 	def index 
 		@posts = Post.all.order('created_at DESC')
@@ -10,18 +12,14 @@ class PostsController < ApplicationController
 
 	end
 	def show
-		@post = Post.find(params[:id])
 	end
 	def edit
-		@post = Post.find(params[:id])
 	end
 	def destroy
-		@post = Post.find(params[:id])
 		@post.destroy
 		redirect_to	root_path
 	end
 	def update
-		@post = Post.find(params[:id])
 		if @post.update(post_param)
 			redirect_to @post
 		else
